3747cab7c26c3a64c7bb9ac180691a6397a4cad7
Luis Jimenez <ljimenez29@gmail.com>
2018-03-20 16:03:39 -0400

    Primer commit

---
+++ app.js
@@ -0,0 +1,37 @@
+//Requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+
+// Inicializar variables
+var app = express();
+
+
+// Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de Datos: \x1b[36m%s\x1b[0m', 'online');
+
+});
+
+
+
+//Rutas
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    });
+});
+
+
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express Server puerto 3000: \x1b[36m%s\x1b[0m', 'online');
+
+});
